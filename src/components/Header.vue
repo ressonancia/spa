82f3071990d418554df383029869df9dc3b21f10
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2025-01-06 23:16:10 -0300

    add gravatar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,13 +18,6 @@
 					</div>
 					<div class="hidden md:block">
 						<div class="ml-4 flex items-center md:ml-6">
-							<button type="button"
-								class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-								<span class="absolute -inset-1.5" />
-								<span class="sr-only">View notifications</span>
-								<BellIcon class="h-6 w-6" aria-hidden="true" />
-							</button>
-
 							<!-- Profile dropdown -->
 							<Menu as="div" class="relative ml-3">
 								<div>
@@ -32,7 +25,7 @@
 										class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 										<span class="absolute -inset-1.5" />
 										<span class="sr-only">Open user menu</span>
-										<img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+										<img class="h-8 w-8 rounded-full" :src="user.avatar" alt="" />
 									</MenuButton>
 								</div>
 								<transition enter-active-class="transition ease-out duration-100"
@@ -79,7 +72,7 @@
 				<div class="border-t border-gray-700 pb-3 pt-4">
 					<div class="flex items-center px-5">
 						<div class="flex-shrink-0">
-							<img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+							<img class="h-10 w-10 rounded-full" :src="user.avatar" alt="" />
 						</div>
 						<div class="ml-3">
 							<div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
@@ -136,6 +129,10 @@ const router = useRouter()
 const route = useRoute()
 const globalStore = useGlobalStore()
 const headerLabel = ref('')
+const user = ref([])
+globalStore.getUser().then((userData) => {
+	user.value = userData
+})
 
 headerLabel.value = globalStore.headerLabel
 
@@ -154,12 +151,6 @@ const logout = () => {
 	})
 }
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
 const navigation = [
   { name: 'Projects', href: getRoutePathByName('projects'), current: route.name == 'projects' },
   { name: 'Create Project', href: getRoutePathByName('create-projects'), current: route.name == 'create-projects' },
